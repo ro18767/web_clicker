6d23b7aebcc21b2306c7c6c3f6407993dc7d3f11
VladyslavGontarenko <122695793+VladyslavGontarenko@users.noreply.github.com>
2023-04-26 17:25:08 +0200

    buttonmusik

--- src/js/SoundSetting/SoundSetting.js
+++ src/js/SoundSetting/SoundSetting.js
@@ -1,11 +1,9 @@
 "use strict";
 
-
 class SoundSetting {
     domElement = document.createElement("div");
-   
+
     constructor() {
         this.domElement.classList.add('SoundSetting');
     }
-
 }
